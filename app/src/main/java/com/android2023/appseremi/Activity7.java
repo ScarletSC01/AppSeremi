0b00bd0fe8f6f32c77849f9debf46f637e5a23ee
RomiGG <f.monsalve7@alumnos.santotomas.cl>
2023-11-09 16:42:29 -0300

    implementacion boton volver activity 7-8

--- app/src/main/java/com/android2023/appseremi/Activity7.java
+++ app/src/main/java/com/android2023/appseremi/Activity7.java
@@ -2,13 +2,27 @@ package com.android2023.appseremi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity7 extends AppCompatActivity {
 
+    Button volver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_7);
+
+        volver = findViewById(R.id.btnVolver);
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity7.this,Activity6.class);
+                startActivity(intent);
+            }
+        });
     }
 }
